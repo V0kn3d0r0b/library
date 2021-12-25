7bbc8b9e08120368a002adff879830cb5a2ea5bf
H3LIGE <82318593+H3LIGE@users.noreply.github.com>
2021-12-25 11:01:07 +0300

    Update library.php

--- library.php
+++ library.php
@@ -34,35 +34,9 @@ $allBook = $users->selectAllBookData();
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
 
         <title>Альбом | Album example for Bootstrap (BS 4.0)</title>
-
-        <!-- Bootstrap core CSS -->
-        <link href="../../../../dist/css/bootstrap.min.css" rel="stylesheet">
-
-        <!-- Custom styles for this template -->
-        <link href="album.css" rel="stylesheet">
     </head>
-
     <body>
-    <script defer>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-        ga('create', 'UA-4481610-59', 'auto');
-        ga('send', 'pageview');
-
-    </script>
-
-    <!-- Yandex.Metrika counter --> <script type="text/javascript" > (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(39705265, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); </script> <noscript><div><img src="https://mc.yandex.ru/watch/39705265" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter -->
-
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <script>
-        (adsbygoogle = window.adsbygoogle || []).push({
-            google_ad_client: "ca-pub-8588635311388465",
-            enable_page_level_ads: true
-        });
-    </script>
     <main role="main">
         <div class="album py-5 bg-light">
             <div class="container">
@@ -92,10 +66,6 @@ $allBook = $users->selectAllBookData();
 
 
         <?php }} ?>
-        <? ?>
-
-
-
 
     </main>
 
